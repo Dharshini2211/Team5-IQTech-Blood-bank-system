acd9168367a7f4719ab83e5a07817ecd6b5f9cc1
VIJAYASRI-M <81587044+VIJAYASRI-M@users.noreply.github.com>
2022-03-04 00:35:26 +0530

    Update jdbcDemo.java

--- jdbcDemo.java
+++ jdbcDemo.java
@@ -1,21 +1,44 @@
 package bloodbanksystem1;
 import java.sql.*;
+import java.util.*;
 
-public class JdbcDemo{
-  public static void main(String args[]){  
-  try{  
-    Class.forName("com.mysql.cj.jdbc.Driver");  
-    Connection con=DriverManager.getConnection("jdbc:mysql://localhost:3306/javajdbc?","root","");
-    Statement stmt=con.createStatement();  
-    ResultSet rs=stmt.executeQuery("select FirstName from bloodbank where ID=1");  
-     rs.next();
-     String name=rs.getString(1);
-      System.out.println(name);
-     st.close();
-     con.close();
-    }catch(Exception e){
-      System.out.println(e);
-    }  
-}  
-}  
+
+
+public class JdbcDemo {
+	public static void main(String args[])throws Exception{
+		System.out.println("Welcome to the Blood Bank System");
+		System.out.println("Select Any required action from the List");
+		System.out.println("Enter the Details of the Donor");
+		Scanner sc=new Scanner(System.in);
+		String firstName=sc.next();
+		String lastName=sc.next();
+		String bloodGroup=sc.next();
+		int age=sc.nextInt();
+		String gender=sc.next();
+		String address=sc.next();
+		String phoneNo=sc.next();
+		String dateOfLastDonation=sc.next();
+		
+	    String url1="jdbc:mysql://localhost:3306/javajdbc?";
+	    String uname1="root";
+		String pass1="";
+		String query="insert into BloodBank(FirstName,LastName,BloodGroup,Age,Gender,Address,PhoneNumber,DateOfLastDonation"+ "values(?,?,?,?,?,?,?,?)";
+			
+		Class.forName("com.mysql.cj.jdbc.Driver");
+		Connection con1 = DriverManager.getConnection(url1,uname1,pass1);
+		
+		PreparedStatement pstmt = con1.prepareStatement(query); 
+		pstmt.setString(1, firstName);
+		pstmt.setString(2, lastName);
+		pstmt.setString(3, bloodGroup);
+		pstmt.setInt(4, age);
+		pstmt.setString(5, gender);
+		pstmt.setString(6, address);
+		pstmt.setString(7, phoneNo);
+		pstmt.setString(8, dateOfLastDonation);
+        System.out.println("Inserted");
+		con1.close();
+		sc.close();
+	}
+
 }
