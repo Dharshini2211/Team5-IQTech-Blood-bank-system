13e0839bf0d1f533e2c75fe0771cd2c336ae98cc
VIJAYASRI-M <81587044+VIJAYASRI-M@users.noreply.github.com>
2022-03-05 16:49:37 +0530

    Update jdbcDemo.java

--- jdbcDemo.java
+++ jdbcDemo.java
@@ -6,39 +6,56 @@ import java.util.*;
 
 public class JdbcDemo {
 	public static void main(String args[])throws Exception{
-		System.out.println("Welcome to the Blood Bank System");
+		System.out.println("-------Welcome to the Blood Bank System--------");
 		System.out.println("Select Any required action from the List");
-		System.out.println("Enter the Details of the Donor");
+		System.out.println("1.Enter the details of the donor\n2.Edit the details of the donor\n3.");
+		
 		Scanner sc=new Scanner(System.in);
-		String firstName=sc.next();
-		String lastName=sc.next();
-		String bloodGroup=sc.next();
-		int age=sc.nextInt();
-		String gender=sc.next();
-		String address=sc.next();
-		String phoneNo=sc.next();
-		String dateOfLastDonation=sc.next();
 		
 	    String url1="jdbc:mysql://localhost:3306/javajdbc?";
-	    String uname1="root";
-		String pass1="";
-		String query="insert into BloodBank(FirstName,LastName,BloodGroup,Age,Gender,Address,PhoneNumber,DateOfLastDonation"+ "values(?,?,?,?,?,?,?,?)";
+	    String uname1="vijayasri";
+		String pass1="Vijaya@02";
+		
 			
 		Class.forName("com.mysql.cj.jdbc.Driver");
 		Connection con1 = DriverManager.getConnection(url1,uname1,pass1);
 		
-		PreparedStatement pstmt = con1.prepareStatement(query); 
-		pstmt.setString(1, firstName);
-		pstmt.setString(2, lastName);
-		pstmt.setString(3, bloodGroup);
-		pstmt.setInt(4, age);
-		pstmt.setString(5, gender);
-		pstmt.setString(6, address);
-		pstmt.setString(7, phoneNo);
-		pstmt.setString(8, dateOfLastDonation);
-        System.out.println("Inserted");
-		con1.close();
-		sc.close();
+		switch(sc.nextInt()) {
+		case 1:
+			System.out.println("Enter the Details of the Donor:");
+			System.out.print("Enter the First Name:");
+			String firstName=sc.next();
+			System.out.print("Enter the Last Name:");
+			String lastName=sc.next();
+			System.out.print("Enter the Blood Group:");
+			String bloodGroup=sc.next();
+			System.out.print("Enter the Age:");
+			int age=sc.nextInt();
+			System.out.print("Enter the Gender:");
+			String gender=sc.next();
+			System.out.print("Enter the Address:");
+			String address=sc.next();
+			System.out.print("Enter the Phone Number:");
+			String phoneNo=sc.next();
+			System.out.print("Enter the Date of Last Donation(YYYY-MM-DD):");
+			String dateOfLastDonation=sc.next();
+			String query="insert into BloodBank(FirstName,LastName,BloodGroup,Age,Gender,Address,PhoneNumber,DateOfLastDonation) values(?,?,?,?,?,?,?,?);";
+			PreparedStatement pstmt = con1.prepareStatement(query); 
+			pstmt.setString(1,firstName);
+			pstmt.setString(2,lastName);
+			pstmt.setString(3,bloodGroup);
+			pstmt.setInt(4,age);
+			pstmt.setString(5,gender);
+			pstmt.setString(6,address);
+			pstmt.setString(7,phoneNo);
+			pstmt.setString(8,dateOfLastDonation);
+			pstmt.executeUpdate();
+			System.out.println("Inserted"); 
+			break;
+		}
+	    con1.close();
+	    sc.close();
+			
 	}
 
 }
