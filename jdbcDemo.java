7b9159b5d5e11eb781bfb7d200bce2fd7696d800
ARIFABANU-M <100758801+ARIFABANU-M@users.noreply.github.com>
2022-03-06 13:44:39 +0530

    Update jdbcDemo.java

--- jdbcDemo.java
+++ jdbcDemo.java
@@ -79,6 +79,9 @@ public class JdbcDemo {
 		pstmt.setInt(2, id);
 		pstmt.executeUpdate();
 		System.out.println("Successfully Updated the Blood Quantity");
+		
+		
+	}
 		public static void EditExpiryDate(Connection con1, Scanner sc)throws Exception {
 		System.out.println("BloodID\nA+ve = 1\nA-ve = 2\nB+ve = 3\nB-ve = 4\nO+ve = 5\nO-ve = 6\nAB+ve = 7\nAB-ve = 8");
 		System.out.println("Enter the BloodId:");
