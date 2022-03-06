2e6a331f6a195f7af4a45a77fef0156226dfa2c7
DHANALAKSHMI-S-2003 <100755006+DHANALAKSHMI-S-2003@users.noreply.github.com>
2022-03-06 14:10:42 +0530

    Update jdbcDemo.java

--- jdbcDemo.java
+++ jdbcDemo.java
@@ -97,7 +97,16 @@ public class JdbcDemo {
 		
 	}
 		
+		public static void DisplayBloodAvailability(Connection con1)throws Exception {
+		Statement stmt=con1.createStatement();
+		String query="select * from BloodAvailable;";
+		ResultSet rs= stmt.executeQuery(query);
+		System.out.println("Blood ID\tBlood Group\tBlood Quantity\t  Expiry Date");
+		while(rs.next()){
+			System.out.println("    "+rs.getInt(1)+"\t            "+rs.getString(2)+"\t    "+rs.getString(3)+"      \t  "+rs.getString(4));
+		}
 		
+	}
 	
 	
 }
